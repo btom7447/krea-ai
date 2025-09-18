0bb3bb97b738c004a46c4c2f8f5510653f1a6006
btom7447 <tombenjamin7447@gmail.com>
2025-09-19 00:54:14 +0100

    removed redundant links

--- src/components/header/HomeDropdown.jsx
+++ src/components/header/HomeDropdown.jsx
@@ -32,7 +32,7 @@ export function HomeDropdown({ onClose }) {
                     </h4>
                     <div className="flex flex-col gap-5">
                         <Link
-                            href="/images"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons img-background text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -42,7 +42,7 @@ export function HomeDropdown({ onClose }) {
                             Images
                         </Link>
                         <Link
-                            href="/video"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons text-white relative aspect-square rounded-[10px] bg-[oklch(79.55%_0.1875_75.3501)] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -50,7 +50,7 @@ export function HomeDropdown({ onClose }) {
                             </div>
                             Video
                         </Link>
-                        <Link href="/realtime"
+                        <Link href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons realTime text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -59,7 +59,7 @@ export function HomeDropdown({ onClose }) {
                             Realtime
                         </Link>
                         <Link
-                            href="/motion-transfer"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons img-background text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -68,7 +68,7 @@ export function HomeDropdown({ onClose }) {
                             Motion Transfer
                         </Link>
                         <Link
-                            href="/3d-objects"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons text-white relative flex aspect-square items-center justify-center rounded-[10px] inset-ring-[0.5px] inset-ring-white/10 size-10.5 shrink-0 bg-[#2f2f2f]">
@@ -86,7 +86,7 @@ export function HomeDropdown({ onClose }) {
                     </h4>
                     <div className="flex flex-col gap-5">
                         <Link
-                            href="/images"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons enhancer text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -95,7 +95,7 @@ export function HomeDropdown({ onClose }) {
                                 Edit
                         </Link>
                         <Link
-                            href="/video"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons edit text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -103,7 +103,7 @@ export function HomeDropdown({ onClose }) {
                             </div>
                             Edit
                         </Link>
-                        <Link href="/realtime"
+                        <Link href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons lipsync text-white relative aspect-square rounded-[10px] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -112,7 +112,7 @@ export function HomeDropdown({ onClose }) {
                             Video Lipsync
                         </Link>
                         <Link
-                            href="/motion-transfer"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <div className="icons relative text-white aspect-square rounded-[10px] bg-[oklch(70%_0.1776_23.29)] inset-ring-[0.5px] shadow-[inset_0_1px_1px_rgba(255,255,255,0.1),inset_0_1px_5px_rgba(255,255,255,0.1)] inset-ring-black/10 size-10.5 shrink-0">
@@ -130,20 +130,20 @@ export function HomeDropdown({ onClose }) {
                     </h4>
                     <div className="flex flex-col gap-5">
                         <Link
-                            href="/images"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <TrainIcon />
                             Train
                         </Link>
                         <Link
-                            href="/video"
+                            href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <AssetsIcon />
                             Assets
                         </Link>
-                        <Link href="/realtime"
+                        <Link href="#"
                             className="flex items-center gap-2 text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition"
                         >
                             <GalleryIcon />
